d6f3db3b80ebc95395e824ce64b8b92263fd514b
Tom Stanton <tomgstanton@gmail.com>
2023-09-02 13:16:39 +0100

    Year 2015, Days 9 and 10

---
+++ 2015/Day 09/code_part2.py
@@ -0,0 +1,53 @@
+import itertools
+
+dir = '2015/Day 09/input.txt'
+string = open(dir,'r')
+inputlines = string.readlines()
+
+class Locations:
+    def __init__(self,):
+        self.locations = []
+        self.functions = []
+
+def LoadLocations(line):
+    pair = [line.split()[0],line.split()[2]]
+    distance = line.split()[4]
+    for x in pair:
+        if x not in cities.locations:
+            cities.locations.append(x)
+    set_pair = {pair[0],pair[1]}
+    cities.functions.append([set_pair,distance])
+
+def GetDistance(set_pair):
+    for function in cities.functions:
+        if set_pair == function[0]:
+            return function[1]
+
+def MeasurePath(x,output,oldpath):
+    path_length = 0
+    newpath = []
+    for index in range(len(x)):
+        if index != 0:
+            set_pair = {x[index-1],x[index]}
+            distance = GetDistance(set_pair)
+            path_length += int(distance)
+            newpath.append([set_pair,distance])
+    if path_length < output:
+        return output, oldpath
+    return path_length, newpath
+
+def Calculate():
+    output = 0
+    path = []
+    for line in inputlines:
+        LoadLocations(line)
+    permutations = itertools.permutations(cities.locations)
+    for x in permutations:
+        output, path = MeasurePath(x,output,path)
+    return output, path
+
+cities = Locations()
+answer, path = Calculate()
+print('Answer is:',answer)
+print('Answer with workings:',path)
+
---
+++ 2015/Day 10/code_part1.py
@@ -0,0 +1,23 @@
+dir = '2015/Day 10/input.txt'
+string = open(dir,'r')
+inputline = string.read()
+
+def ElfSay(line):
+    output = ''
+    count = 1
+    for index in range(len(line)):
+        if index !=0 and line[index-1] == line[index]:
+            count += 1
+        if index !=0 and line[index-1] != line[index]:
+            output += str(count)+line[index-1]
+            count = 1
+    output += str(count)+line[-1]        
+    return output
+
+def Calculate(line):
+    for x in range(40):
+        line = ElfSay(line)
+    return len(line)
+
+answer = Calculate(inputline)
+print('Answer is:',answer)
